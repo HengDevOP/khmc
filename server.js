5c64d875c26b2ba508b37954098c262bcdae7edd
Heng Developer <66843542+HengDevOP@users.noreply.github.com>
2026-03-28 14:01:23 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "127.0.0.1";
+const DISCORD_INVITE_URL =
+  process.env.DISCORD_INVITE_URL || "https://discord.gg/P7tQdXkwEX";
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/discord", (req, res) => {
+  res.redirect(DISCORD_INVITE_URL);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`KHMC.NET server running on http://${HOST}:${PORT}`);
+});
